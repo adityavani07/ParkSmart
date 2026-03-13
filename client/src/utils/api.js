b470f0d713f8f30d195d7599c26b85d100ea80db
thekynoxninjahattori <workforsmartech@gmail.com>
2026-03-13 11:08:03 +0530

    connect frontend to deployed backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://parksmart.onrender.com/api',
+  baseURL: 'https://parksmart-g4ir.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 

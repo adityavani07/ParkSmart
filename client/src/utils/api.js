5fdb9ef7c6fecd06bbfcf37148dc3f2aed8a91c6
thekynoxninjahattori <workforsmartech@gmail.com>
2026-03-13 11:33:12 +0530

    fix api base url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,12 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://parksmart-g4ir.onrender.com/api",
-  headers: { "Content-Type": "application/json" }
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
@@ -15,9 +17,9 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      window.location.href = '/login';
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/login";
     }
     return Promise.reject(error);
   }
